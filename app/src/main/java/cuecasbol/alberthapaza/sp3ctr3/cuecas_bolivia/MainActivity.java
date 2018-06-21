019700ea0563b1a9632145c9d819f3960708977a
alberth <maikeru.bash@gmail.com>
2018-06-21 22:10:45 +0000

    proyecto cuecas creado

---
+++ app/src/main/java/cuecasbol/alberthapaza/sp3ctr3/cuecas_bolivia/MainActivity.java
@@ -0,0 +1,13 @@
+package cuecasbol.alberthapaza.sp3ctr3.cuecas_bolivia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
